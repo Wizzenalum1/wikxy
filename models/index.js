f02c8af57a8f3406e6e8ef3eee06843147ce6715
ghanshyam <ghanshyam.sikri@gmail.com>
2021-08-26 11:45:15 +0530

    like functionallity working with http request

--- models/index.js
+++ models/index.js
@@ -3,4 +3,6 @@ module.exports = {
     User:require('./user_model'),
     Post:require('./post'),
     Comment:require('./comment'),
+    Like:require('./like'),
+    VerifyToken:require('./verify_token')
 }
